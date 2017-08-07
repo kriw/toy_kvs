bab8d5c4c0c032d0031abf7044484df853632b33
kriw <kotarou777775@gmail.com>
2017-08-07 15:29:36 +0900

    add log.go

---
+++ server/scanLog/log.go
@@ -0,0 +1,38 @@
+package scanLog
+
+import (
+	"../../util"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+	"time"
+)
+
+var logger log.Logger
+var logFile *os.File
+
+func Write(ruleName string, hash [util.HashSize]byte) {
+	hashStr := fmt.Sprintf("%x", hash)
+	logger.Printf("ruleName:%s, hash:%s\n", ruleName, hashStr)
+}
+
+func ChangeLogger() {
+	logFile.Close()
+	NewLogger()
+}
+
+func NewLogger() {
+	fileName := strings.Replace(time.Now().String(), " ", "_", -1)
+	fileName = strings.Replace(fileName, "-", "_", -1)
+	fileName = fmt.Sprintf("logFiles/since_%s.log", fileName)
+	logFile, err := os.OpenFile(fileName, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("error opening file: %v", err)
+	}
+	logger.SetOutput(logFile)
+}
+
+func CloseLog() {
+	logFile.Close()
+}
